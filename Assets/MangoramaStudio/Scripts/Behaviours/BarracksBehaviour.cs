77609e47319747e4642026f4a2734c616f206d45
Ali Murat Taşin <alimurat_tasin@hotmail.com>
2023-02-03 17:24:52 +0300

    barracks behaviour created

---
+++ Assets/MangoramaStudio/Scripts/Behaviours/BarracksBehaviour.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarracksBehaviour : MonoBehaviour
+{
+    private void Start()
+    {
+        InputController.OnDrag += Drag;
+    }
+
+    private void Drag(Vector2 dragVector)
+    {
+        transform.position += new Vector3(dragVector.x * 100f, dragVector.y * 100f) * Time.deltaTime;
+    }
+
+    private void OnDestroy()
+    {
+        InputController.OnDrag -= Drag;
+    }
+}
