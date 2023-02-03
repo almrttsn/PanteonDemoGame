19940f74d920fec06efaa28340d2419c5381ed35
Ali Murat Taşin <alimurat_tasin@hotmail.com>
2023-02-03 12:28:30 +0300

    barracks creator utility created

---
+++ Assets/MangoramaStudio/Scripts/Utilities/BarracksCreatorUtility.cs
@@ -0,0 +1,53 @@
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarracksCreatorUtility : MonoBehaviour
+{
+    [SerializeField] BlockBehaviour _barracksMaterial;
+    [SerializeField] int _gridWidthPositive;
+    [SerializeField] int _gridHeightPositive;
+    [SerializeField] int _gridWidthNegative;
+    [SerializeField] int _gridHeightNegative;
+    [SerializeField] float _spacing = 1.12f;
+
+    [Button]
+    private void CreateGrid()
+    {
+        var parentObject = new GameObject();
+        parentObject.name = "Barracks";
+
+        for (int i = 0; i < _gridHeightPositive; i++)
+        {
+            for (int j = 0; j < _gridWidthPositive; j++)
+            {
+                var block = Instantiate(_barracksMaterial, new Vector3((_gridHeightPositive - i) * _spacing - 0.56f, (_gridWidthPositive - j) * _spacing - 0.56f, -0.5f), Quaternion.identity, parentObject.transform);
+            }
+        }
+
+        for (int i = _gridHeightNegative; i < 0; i++)
+        {
+            for (int j = _gridWidthNegative; j < 0; j++)
+            {
+                var block = Instantiate(_barracksMaterial, new Vector3((_gridHeightNegative - i) * _spacing - 0.56f, (_gridWidthNegative - j) * _spacing - 0.56f, -0.5f), Quaternion.identity, parentObject.transform);
+            }
+        }
+
+        for (int i = 0; i < _gridHeightPositive; i++)
+        {
+            for (int j = _gridWidthNegative; j < 0; j++)
+            {
+                var block = Instantiate(_barracksMaterial, new Vector3((_gridHeightPositive - i) * _spacing - 0.56f, (_gridWidthNegative - j) * _spacing - 0.56f, -0.5f), Quaternion.identity, parentObject.transform);
+            }
+        }
+
+        for (int i = _gridHeightNegative; i < 0; i++)
+        {
+            for (int j = 0; j < _gridWidthPositive; j++)
+            {
+                var block = Instantiate(_barracksMaterial, new Vector3((_gridHeightNegative - i) * _spacing - 0.56f, (_gridWidthPositive - j) * _spacing - 0.56f, -0.5f), Quaternion.identity, parentObject.transform);
+            }
+        }
+    }
+}
