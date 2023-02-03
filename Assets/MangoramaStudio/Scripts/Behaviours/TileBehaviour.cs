ed1827ad53d1e3789ba50e0de48fc82bfda1a43e
Ali Murat Taşin <alimurat_tasin@hotmail.com>
2023-02-03 10:12:05 +0300

    tile behaviour created

---
+++ Assets/MangoramaStudio/Scripts/Behaviours/TileBehaviour.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class TileBehaviour : MonoBehaviour
+{
+    public int Row { get; set; }
+    public int Column { get; set; }
+    public BlockBehaviour CurrentBlockBehaviour { get; set; }
+    public bool IsOccupied;
+}
