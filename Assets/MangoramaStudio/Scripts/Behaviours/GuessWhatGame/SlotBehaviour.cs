8e6f51abad1431d9a15f14728a233464c9e1f1a2
Ali Murat Taşin <alimurat_tasin@hotmail.com>
2023-02-06 17:09:06 +0300

    slot behaviour created

---
+++ Assets/MangoramaStudio/Scripts/Behaviours/GuessWhatGame/SlotBehaviour.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlotBehaviour : MonoBehaviour
+{
+    public TextMesh Text => _text;
+    [SerializeField] private TextMesh _text;
+
+    private void Start()
+    {
+        _text = GetComponentInChildren<TextMesh>();
+        _text.text = gameObject.tag.ToString();   
+    }
+}
