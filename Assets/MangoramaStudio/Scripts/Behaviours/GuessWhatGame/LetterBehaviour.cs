eefca6fb36da97c85b6b08752fdcac178adf108a
Ali Murat Taşin <alimurat_tasin@hotmail.com>
2023-02-10 18:03:36 +0300

    click lock operation arrangement

--- Assets/MangoramaStudio/Scripts/Behaviours/GuessWhatGame/LetterBehaviour.cs
+++ Assets/MangoramaStudio/Scripts/Behaviours/GuessWhatGame/LetterBehaviour.cs
@@ -6,19 +6,24 @@ using UnityEngine;
 
 public class LetterBehaviour : MonoBehaviour
 {
-
+    public bool PlayerCanClick;
     public event Action<LetterBehaviour, Vector3> OnClickedALetter;
     [SerializeField] private TextMesh text;
+    [SerializeField] private LetterController _letterController;
 
     private void Start()
     {
         text.text = gameObject.tag.ToString();
+        PlayerCanClick = true;
     }
 
     private void OnMouseDown()
     {
-        OnClickedALetter?.Invoke(this, transform.position);
-        gameObject.SetActive(false);
-        PlayerData.PlayerMoney -= 100;
+        if (PlayerCanClick == true)
+        {
+            OnClickedALetter?.Invoke(this, transform.position);
+            gameObject.SetActive(false);
+            PlayerData.PlayerMoney -= 100;
+        }
     }
 }
